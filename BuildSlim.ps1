7a27b289dec16491da59728f55cf783a1a2fdef6
Akatsuki <mldkyt@proton.me>
2024-05-08 10:42:21 +0200

    Fix cursor

--- BuildSlim.ps1
+++ BuildSlim.ps1
@@ -1,13 +1,18 @@
 
 # Slim Build script
 # Generates the site HTML without images, etc.
+# Useful when skipping uploading of images, etc.
 
 # Start by building
 yarn.cmd
 yarn.cmd build
 
+Remove-Item -Recurse -Path .\dist2
+
+Copy-Item -Recurse -Path .\dist -Destination .\dist2\
+
 # Change folder
-Set-Location -Path .\dist
+Set-Location -Path .\dist2
 
 # Delete unnecessary files
 Get-ChildItem -Recurse -File *.webp | Remove-Item
@@ -17,6 +22,9 @@ Get-ChildItem -Recurse -File *.png | Remove-Item
 Get-ChildItem -Recurse -File *.jpg | Remove-Item
 Get-ChildItem -Recurse -File *.jpeg | Remove-Item
 Get-ChildItem -Recurse -File *.svg | Remove-Item
+Remove-Item -Recurse -Path '_astro'
 
 # Compress
-Compress-Archive -Path . -DestinationPath dist.zip
+Compress-Archive -Path * -DestinationPath dist.zip
+
+Set-Location -Path ..
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://akatsuki.nekoweb.org'
+    site: 'https://akatsuki.nekoweb.org',
+    compressHTML: true
 });
